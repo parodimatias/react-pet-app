a125c828bb5c4492721d64f33134ea19abcaec39
Matias <matias.parodi@outlook.com>
2022-03-19 16:56:51 -0300

    change to local server

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { AppRoutes } from "./router";
-// export const API_BASE_URL = "http://localhost:3000";
-export const API_BASE_URL = "https://matias-pet-finder-app.herokuapp.com";
+export const API_BASE_URL = "http://localhost:3000";
+// export const API_BASE_URL = "https://matias-pet-finder-app.herokuapp.com";
 
 function App() {
   return (
